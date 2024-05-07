6a81fcac253906b15f9067a0a9757ded3cdb80cd
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-07 14:31:46 -0700

    comment out config file and keep in server.js. i express

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
 // Connects JavaScript and Sequelize
 
-const sequelize = require("sequelize");
-require("dotenv").config();
+// const sequelize = require("sequelize");
+// require("dotenv").config();
 
-sequelize.connect (
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "127.0.0.1",
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    dialect: "mysql",
-    port: 3306,
-  },
-  db.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to the employee database!");
-  })
-);
+// sequelize.connect (
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: process.env.DB_PASSWORD,
+//     dialect: "mysql",
+//     port: 3306,
+//   },
+//   db.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected to the employee database!");
+//   })
+// );
 
-module.exports = sequelize;
+// module.exports = sequelize;
 
 // const mysql = require("mysql2");
 // const db = mysql.createConnection(
--- db/schema.sql
+++ db/schema.sql
@@ -9,22 +9,17 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL   
+    role_id INT NOT NULL,
+    manager_id INT
 );
